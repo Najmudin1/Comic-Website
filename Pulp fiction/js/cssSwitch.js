cfd4f10f58b76187bfd58ed4692ca0d59e15c31a
Najmudin Saqib <P15225267@LEC-ADMIN.dmu.ac.uk>
2017-11-16 10:39:40 +0000

    finished Comic

---
+++ Pulp fiction/js/cssSwitch.js
@@ -0,0 +1,7 @@
+function SwitchCss() {
+	document.getElementById('mainCss').href='../css/stylesheet2.css';
+}
+function OrignalCss() {
+	document.getElementById('mainCss').href='../css/stylesheet1.css';
+}
+
